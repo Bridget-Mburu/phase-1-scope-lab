1345491c17ca49154afe7a86473311e2bc0b8e48
bridget <bridget.mburu.w@gmail.com>
2024-07-23 21:57:49 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+ var customerName = 'bob'
+
+ //global scope
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase() 
+}
+console.log(upperCaseCustomerName)
+
+
+var bestCustomer = undifined
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'     
+}
+setBestCustomer()
+
+ 
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob'
+   }
+overwriteBestCustomer()
+
+
+
+function changeLeastFavouriteCustomer () {
+    const leastFavoriteCustomer = 'tot'
+      leastFavoriteCustomer = 'dan'
+}
+console.log(leastFavouriteCustomer)
